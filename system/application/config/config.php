edd476f42c091a136888a9832534a76dfe0d4213
Michal Hrusecky <Michal@Hrusecky.net>
2010-08-09 18:24:21 +0200

    No hardcoded URL
    As wee support both http://susepaste.org and http://paste.opensuse.org,
    we need to get URL on the way.

--- system/application/config/config.php
+++ system/application/config/config.php
@@ -11,7 +11,7 @@
 |	http://www.example.com/
 |
 */
-$config['base_url']	= "http://susepaste.org/";
+$config['base_url']	= "http://" . $_SERVER["HTTP_HOST"];
 
 /*
 |--------------------------------------------------------------------------
